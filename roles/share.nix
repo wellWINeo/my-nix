4bd5f95711bbfb9345f7d854ade0edf02247ad8f
wellWINeo <usa200313@gmail.com>
2024-04-11 10:54:39 +0300

    add "force user" and "force group" for public share

--- roles/share.nix
+++ roles/share.nix
@@ -48,6 +48,8 @@ in {
           "guest ok" = "yes";
           "create mask" = "0644";
           "directory mask" = "0755";
+          "force user" = "nobody";
+          "force group" = "nogroup";
         };
 
         Homes = {

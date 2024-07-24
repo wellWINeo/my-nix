48442da42f916cca02cedb1ead3cd357a7c16751
wellWINeo <stepan@uspenskiy.su>
2024-07-24 19:17:51 +0300

    few fixes

--- roles/vault.nix
+++ roles/vault.nix
@@ -19,7 +19,7 @@ in {
       config = {
         WEB_VAULT_FOLDER = "${pkgs.bitwarden_rs-vault}/share/vaultwarden/vault";
         WEB_VAULT_ENABLED = true;
-        DATA_DIR = "/var/lib/vault";
+        DATA_FOLDER = "/var/lib/vault";
         IP_HEADER = "X-Real-IP";
         LOG_FILE = "/var/log/vaultwarden";
         WEBSOCKET_ENABLED = true;
@@ -29,6 +29,16 @@ in {
         DOMAIN = "https://vault.${cfg.baseDomain}";
         ROCKET_ADDRESS = "127.0.0.1";
         ROCKET_PORT = 8180;
+        SMTP_HOST = mail.uspenskiy.su;
+        SMTP_FROM = "vault@uspenskiy.su";
+        SMTP_FROM_NAME = "Vaultwarden";
+        SMTP_PORT = 587;
+        SMTP_SSL = true;
+        SMTP_USERNAME = "vault";
+        SMTP_AUTH_MECHANISM = "Plain";
+        SMTP_TIMEOUT = 15;
+        INVITATIONS_ALLOWED = true;
+        SIGNUPS_ALLOWED = false;
       };
 
       environmentFile = "/etc/nixos/secrets/vaultwarden.secret.env";

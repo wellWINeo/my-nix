3760e651bfee8266825aa441ff43fc2acc3b7387
wellWINeo <stepan@uspenskiy.su>
2025-05-04 12:54:39 +0300

    use multiple filters in nginx jail

--- common/hardened.nix
+++ common/hardened.nix
@@ -14,6 +14,7 @@ in {
     services.fail2ban = {
       enable = true;
       maxretry = 5;
+
       jails = {
         sshd = {
           settings = {
@@ -22,35 +23,17 @@ in {
             logpath = "/var/log/auth.log";
           };
         };
+
         nginx = {
           settings = {
-            port = "http,https";
-            filter = "nginx-http-auth";
-            backend = "systemd";
-          };
-        };
-
-        nginx-badbots = {
-          settings = {
-            port = "http,https";
-            filter = "nginx-badbots";
-            backend = "systemd";
-          };
-        };
-
-        nginx-noscript = {
-          settings = {
-            port = "http,https";
-            filter = "nginx-noscript";
-            backend = "systemd";
-          };
-        };
-
-        nginx-proxy = {
-          settings = {
-            port = "http,https";
-            filter = "nginx-proxy";
-            backend = "systemd";
+            port= "http,https";
+            filter = concatStringsSep ", " [ 
+              "nginx-http-auth"
+              "nginx-bad-request"
+              "nginx-botsearch"
+              "nginx-error-common"
+              "nginx-forbidden"
+            ];
           };
         };
       };

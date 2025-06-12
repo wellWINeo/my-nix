a0b04a8283bc0b692287b9b98aa5c38ef5413956
wellWINeo <stepan@uspenskiy.su>
2025-06-12 12:50:33 +0300

    explicitly define user and group

--- roles/torrent.nix
+++ roles/torrent.nix
@@ -12,31 +12,38 @@ let
   defineMediaUser = import ../common/define-media-user.nix;
 
   torrentUser = "torrent";
+  mediaGroup = "media";
   cfg = config.roles.torrent;
   baseDir = "/mnt/storage/Public/Torrents";
 in
 {
   options.roles.torrent.enable = mkEnableOption "Enable Transmission";
 
-  config =
-    mkIf cfg.enable {
-      environment.systemPackages = with pkgs; [ transmission_4 ];
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [ transmission_4 ];
 
-      # services.transmission = {
-      #   enable = true;
-      #   package = pkgs.transmission_4;
-      #   openFirewall = true;
-      #   openRPCPort = true;
-      #   downloadDirPermissions = "777";
-      #   user = torrentUser;
-      #   group = "media";
-      #   settings = {
-      #     incomplete-dir = "${baseDir}/Incomplete";
-      #     download-dir = "${baseDir}/Downloads";
-      #     rpc-bind-address = "0.0.0.0";
-      #     rpc-whitelist = "127.0.0.1,192.168.0.*,10.20.0.*";
-      #   };
-      # };
-    }
-    // defineMediaUser torrentUser;
+    users.groups.${mediaGroup} = { };
+    users.users.${torrentUser} = {
+      isNormalUser = false;
+      isSystemUser = true;
+
+      extraGroups = [ mediaGroup ];
+    };
+
+    # services.transmission = {
+    #   enable = true;
+    #   package = pkgs.transmission_4;
+    #   openFirewall = true;
+    #   openRPCPort = true;
+    #   downloadDirPermissions = "777";
+    #   user = torrentUser;
+    #   group = "media";
+    #   settings = {
+    #     incomplete-dir = "${baseDir}/Incomplete";
+    #     download-dir = "${baseDir}/Downloads";
+    #     rpc-bind-address = "0.0.0.0";
+    #     rpc-whitelist = "127.0.0.1,192.168.0.*,10.20.0.*";
+    #   };
+    # };
+  };
 }

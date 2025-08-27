5c4e9aec800f02c4a847c72b55f882317e335cbb
wellWINeo <stepan@uspenskiy.su>
2025-08-27 21:48:30 +0300

    remove unused configs

--- roles/router/dns.nix
+++ roles/router/dns.nix
@@ -1,6 +1,5 @@
 {
   config,
-  pkgs,
   lib,
   ...
 }:
@@ -39,17 +38,6 @@ in
       nameservers = optionals cfg.useLocalDNS [ "127.0.0.1" ];
     };
 
-    # services.dnsmasq = {
-    #   enable = true;
-    #   settings = {
-    #     no-resolv = true;
-    #     server = [ "127.0.0.1#8053" ];
-    #     address = ''
-    #       /photos.nixpi/${cfg.ipAddress}
-    #     '';
-    #   };
-    # };
-
     services.coredns = {
       enable = true;
       config = ''
@@ -80,7 +68,7 @@ in
         #    health_check 5s 
         #  }
         #} 
-        
+
         .:9055 {
           forward . tls://1.1.1.1 tls://1.0.0.1 {
             tls_servername cloudflare-dns.com
@@ -94,7 +82,7 @@ in
         #    health_check 5s 
         #  } 
         #}
-        
+
         .:9057 {
           forward . tls://8.8.8.8 tls://8.8.4.4 {
             tls_servername dns.google
@@ -118,36 +106,6 @@ in
           cache
         }
       '';
-    };
-
-    services.stubby = {
-      enable = false;
-      settings = pkgs.stubby.passthru.settingsExample // {
-        listen_addresses = [ "127.0.0.1@8053" ];
-        upstream_recursive_servers = [
-          {
-            address_data = "1.1.1.1";
-            tls_auth_name = "cloudflare-dns.com";
-            tls_pubkey_pinset = [
-              {
-                digest = "sha256";
-                value = "SPfg6FluPIlUc6a5h313BDCxQYNGX+THTy7ig5X3+VA=";
-              }
-            ];
-          }
-
-          {
-            address_data = "1.0.0.1";
-            tls_auth_name = "cloudflare-dns.com";
-            tls_pubkey_pinset = [
-              {
-                digest = "sha256";
-                value = "SPfg6FluPIlUc6a5h313BDCxQYNGX+THTy7ig5X3+VA=";
-              }
-            ];
-          }
-        ];
-      };
     };
   };
 }

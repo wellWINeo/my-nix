f8426c0d0f27e73af451367672493998c3fdb43a
wellWINeo <stepan@uspenskiy.su>
2026-02-23 12:41:21 +0300

    revert(sing-box): remove duplicate TCP 443 firewall rule

--- roles/network/sing-box/server.nix
+++ roles/network/sing-box/server.nix
@@ -131,10 +131,6 @@ in
       settings = singBoxConfig;
     };
 
-    networking.firewall.allowedTCPPorts = lib.optionals (cfg.vlessWs.enable || cfg.vlessGrpc.enable) [
-      443
-    ];
-
     services.nginx = mkIf (cfg.vlessWs.enable || cfg.vlessGrpc.enable) {
       enable = true;
 

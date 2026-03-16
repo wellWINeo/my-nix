bf0f2195da2b15da76a9375e146400f249e4d643
wellWINeo <stepan@uspenskiy.su>
2026-03-16 22:08:06 +0300

    feat: add home-manager flake input

--- flake.nix
+++ flake.nix
@@ -4,6 +4,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
